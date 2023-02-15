e94af739d6fb769482ddd54fb32d4b1fc8cc9984
Romulo Rosa <romulo.frontend@gmail.com>
2023-02-15 12:08:58 -0300

    aula 02 js operadores numbers strings

---
+++ aula-02-numeros-operadores/js/app.js
@@ -0,0 +1,25 @@
+
+//MULTIPLICAÇÃO
+var operador = 100;
+var operando = 50;
+var resultado = operador * operando;
+console.log(resultado)
+
+
+
+//SOMA (para numbers) OU CONCATENAÇÃO (para strings)
+var nome = "Romulo";
+var sobrenome = "Rosa";
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
+
+
+
+//SUBTRAÇÃO
+// Js não subtrai numbers de strings de texto ou vice-versa
+// Js subtrai numbers de numbers
+// Js subtrai numbers de strings com texto numeérico
+var primeiroValor = 10;
+var segundoValor = 5;
+var resultadoSubtracao = primeiroValor - segundoValor;
+console.log(resultadoSubtracao);
